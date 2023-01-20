edfee45f44ef833830d4842c59716833a49b050d
L-Caro <lionelcaro74@gmail.com>
2023-01-20 20:28:42 +0100

    Fond ecran et couleur js faite

--- Page principale/JS/script.js
+++ Page principale/JS/script.js
@@ -18,34 +18,34 @@ function closeNav() {
 // large mode //
 document.getElementById("switchTheme").addEventListener("change", function() {
   if (document.getElementById("switchTheme").checked == false) {
-    document.documentElement.setAttribute("style", "background-color: #121212;");
+    document.documentElement.setAttribute("style", "background-color: #7D7D7D;");
     document.getElementsByTagName("body")[0].setAttribute("style", "color: #c7c7c7;");
 
   } else {
-    document.documentElement.setAttribute("style", "background-color: white;");
-    document.getElementsByTagName("body")[0].setAttribute("style", "color: black;");
+    document.documentElement.setAttribute("style", "background-color: #C7C7C7;");
+    document.getElementsByTagName("body")[0].setAttribute("style", "color: #7D7D7D;");
   }
 });
 document.getElementById("switchTheme").addEventListener("change", function() {
   if (document.getElementById("switchTheme").checked == false) {
-    document.documentElement.setAttribute("style", "background-color: #121212;");
+    document.documentElement.setAttribute("style", "background-color: #7D7D7D;");
     document.getElementsByTagName("nav")[0].setAttribute("style", "color: #c7c7c7;");
 
   } else {
-    document.documentElement.setAttribute("style", "background-color: white;");
-    document.getElementsByTagName("nav")[0].setAttribute("style", "color: black;");
+    document.documentElement.setAttribute("style", "background-color: #C7C7C7;");
+    document.getElementsByTagName("nav")[0].setAttribute("style", "color: #7D7D7D;");
   }
 });
 
 // small mode //
 document.getElementById("switchTheme2").addEventListener("change", function() {
   if (document.getElementById("switchTheme2").checked == false) {
-    document.documentElement.setAttribute("style", "background-color: #121212;");
+    document.documentElement.setAttribute("style", "background-color: #7D7D7D;");
     document.getElementsByTagName("body")[0].setAttribute("style", "color: #c7c7c7;");
 
   } else {
-    document.documentElement.setAttribute("style", "background-color: white;");
-    document.getElementsByTagName("body")[0].setAttribute("style", "color: black;");
+    document.documentElement.setAttribute("style", "background-color: #c7C7C7;");
+    document.getElementsByTagName("body")[0].setAttribute("style", "color: #7D7D7D;");
   }
 });
 // == == //
